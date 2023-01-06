256b72c2defbbacca9c331269fa29a0d82564025
Jairo Caycho - CEO <jairo@gluon.pe>
2023-01-06 11:44:45 +0100

    tested implementation of modifier

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 from connector import lab_client, collection_planets, collection_comets
 from retriver import selector
 from inserter import writer
+from modifier import updater
 
 
 def run():
     selector_result = selector(collection_planets)
-    writer_result = writer(collection_comets)
     for _ in selector_result:
         print(_)
+    writer_result = writer(collection_comets)
     print(writer_result.inserted_ids)
+    updater_result = updater(collection_comets)
+    print(updater_result.modified_count)
     lab_client.close()
